7845d237cd9e2c6971511f7892f72cbb9919c992
Madhubalajb <jbmadhumng@gmail.com>
2021-05-02 10:16:21 +0530

    Hash Table

--- src/components/HashTable.js
+++ src/components/HashTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Table } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const HashTable = () => {
@@ -7,6 +7,33 @@ const HashTable = () => {
         <Container>
             <Link to="/">Home</Link>
             <h1>Hash Table</h1>
+            <p className="lead">
+                Hash table ia an array of fixed size where data will be stored in key-value manner. The key will be mapped to an array index
+                in the range of <mark>0</mark> to <mark>Tablesize - 1</mark>. The mapping is done by <mark>Hash-Function</mark>. And thus value
+                will be stored in the respective mapped cell.
+            </p>
+            <p>Let's say we are gonna store the salary information, the key and values are</p>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>Key</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Bruce</td>
+                        <td>40000</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>Clark</td>
+                        <td>50000</td>
+                    </tr>
+                </tbody>
+            </Table>
+            <p>The Hash Function will distribute the keys evenly among the cells.</p>
         </Container>
     )
 }
